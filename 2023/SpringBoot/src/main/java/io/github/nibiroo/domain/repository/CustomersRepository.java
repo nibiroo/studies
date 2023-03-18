de99aa133c0fdbfe8bca74259c7b991563c5de73
Guilherme Carvalho <55836497+nibiroo@users.noreply.github.com>
2023-03-18 14:32:57 -0300

    [aula-25] - deleting query methods jpa examples

--- 2023/SpringBoot/src/main/java/io/github/nibiroo/domain/repository/CustomersRepository.java
+++ 2023/SpringBoot/src/main/java/io/github/nibiroo/domain/repository/CustomersRepository.java
@@ -9,9 +9,5 @@ public interface CustomersRepository extends JpaRepository<Customer, Integer> {
 
     List<Customer> findByNameContains(String name);
 
-    List<Customer> findByNameOrId(String name, Integer id);
-
-    Customer findOneByName(String name);
-
     Boolean existsByName(String name);
 }
