a24a466a4ff176afbe68488dcfc01bd6e989e5cd
Guilherme Carvalho <55836497+nibiroo@users.noreply.github.com>
2023-03-20 21:44:12 -0300

    delete import not used

--- 2023/SpringBoot/src/main/java/io/github/nibiroo/SalesApplication.java
+++ 2023/SpringBoot/src/main/java/io/github/nibiroo/SalesApplication.java
@@ -1,20 +1,9 @@
 package io.github.nibiroo;
 
 
-import io.github.nibiroo.domain.entity.Customer;
-import io.github.nibiroo.domain.entity.Invoice;
-import io.github.nibiroo.domain.repository.CustomersRepository;
-import io.github.nibiroo.domain.repository.InvoiceRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
 
 // ANNOTATIONS
 //   SpringBootApplication: Means this Java class is an SpringBoot application
