6356ca817fbec315f47c1d3e75d4315361d3fecb
Guilherme Carvalho <55836497+nibiroo@users.noreply.github.com>
2023-03-26 17:35:41 -0300

    Enhancement

--- 2023/SpringBoot/src/main/java/io/github/nibiroo/SalesApplication.java
+++ 2023/SpringBoot/src/main/java/io/github/nibiroo/SalesApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SalesApplication {
-
-    // PSVM - Public Static Void Main, to create faster
     public static void main(String[] args) {
         SpringApplication.run(SalesApplication.class, args);
     }
