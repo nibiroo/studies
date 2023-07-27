ec87ae5a60ad9a8cdbd766bbf104bb95947379b5
Guilherme Machado <55836497+nibiroo@users.noreply.github.com>
2023-07-26 22:36:14 -0300

    [aula-68] - Criando a classe de configuração do Security

---
+++ 2023/SpringBoot/src/main/java/io/github/nibiroo/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package io.github.nibiroo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // configuração ...
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // configuração ...
+    }
+}
