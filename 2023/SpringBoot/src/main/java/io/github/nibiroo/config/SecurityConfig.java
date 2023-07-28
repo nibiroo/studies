2e51773d94b89fd4383a0961ce6ceeedc4e6dc0b
Guilherme Machado <55836497+nibiroo@users.noreply.github.com>
2023-07-27 22:27:50 -0300

    [ENHANCEMENT] - Refactoring to Spring Security 6
    It's correct.

--- 2023/SpringBoot/src/main/java/io/github/nibiroo/config/SecurityConfig.java
+++ 2023/SpringBoot/src/main/java/io/github/nibiroo/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package io.github.nibiroo.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
@@ -17,12 +23,32 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // configuração ...
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(
+                        authorizeConfig -> {
+                            authorizeConfig.requestMatchers("/public").permitAll();
+                            authorizeConfig.requestMatchers("/logout").permitAll();
+                            authorizeConfig.anyRequest().authenticated();
+                        }
+                )
+                .formLogin(Customizer.withDefaults())
+                .build();
     }
 
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        // configuração ...
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.builder()
+                                            .username("nibiro")
+                                            .password(passwordEncoder().encode("123"))
+                                            .roles("USER")
+                                            .build();
+        UserDetails admin = User.builder()
+                                        .username("admin")
+                                        .password(passwordEncoder().encode("admin"))
+                                        .roles("ADMIN")
+                                        .build();
+
+        return new InMemoryUserDetailsManager(userDetails, admin);
     }
 }
